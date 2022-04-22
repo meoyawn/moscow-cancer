a2ae97e98cbb0488cc864add95fd422b2425a80e
adelnizamutdinov <stiggpwnz@gmail.com>
2022-04-22 14:26:56 +0400

    amen

--- src/moscow/Moscow.tsx
+++ src/moscow/Moscow.tsx
@@ -14,6 +14,7 @@ import turfCentroid from "@turf/centroid"
 import russia from "./russia.json"
 import years from "./years.json"
 import countries from "./countries.json"
+import states from "./states.json"
 
 type RegionISO = keyof typeof years
 
@@ -42,7 +43,8 @@ type Tooltip =
 const name = (p: GeoProps) => ("ISO_A2" in p ? p.ADMIN : p.shapeName)
 const iso = (p: GeoProps) => ("ISO_A2" in p ? p.ISO_A2 : p.shapeISO)
 
-const getText = (p: GeoProps) => `${name(p)}\n${years[iso(p)]}`
+const getText = (p: GeoProps) =>
+  iso(p) in years ? `${name(p)}\n${years[iso(p)]}` : name(p)
 
 const getTooltip = ({ object }: PickInfo<Feat>): Tooltip =>
   object?.properties ? { text: getText(object?.properties) } : null
@@ -53,7 +55,7 @@ const polygonsStroke: PathLayerProps<Feature<LineString>> = {
   widthMinPixels: 0.1,
 }
 
-const moscowYear = years["RU-MOW"][0]
+const moscowYear = years["RU-MOS"][0]
 const nowYear = new Date().getFullYear()
 const moscowFromNow = nowYear - moscowYear
 
@@ -70,18 +72,35 @@ const getFillColor = ({ properties }: Feat): RGBAColor => {
   if (arr?.length === 1) {
     return toRGB(hsl(0, 0.75, (arr[0] - moscowYear) / moscowFromNow))
   }
-  return [0, 0, 0, 0]
+  return [100, 100, 100]
 }
 
 const data = (
-  [...russia.features, ...countries.features] as ReadonlyArray<Feat>
+  [...russia.features, ...countries.features, ...states.features] as Feat[]
 ).filter((f) => iso(f.properties) in years)
 
 export const Moscow = (): JSX.Element => (
   <DeckGL
     views={[new GlobeView({ resolution: 10 })]}
     layers={[
+      // new SolidPolygonLayer<[number, number][][]>({
+      //   id: "background",
+      //   data: [
+      //     [
+      //       [-180, 90],
+      //       [0, 90],
+      //       [180, 90],
+      //       [180, -90],
+      //       [0, -90],
+      //       [-180, -90],
+      //     ],
+      //   ],
+      //   getPolygon: (d) => d,
+      //   filled: true,
+      //   getFillColor: [100, 100, 100],
+      // }),
       new GeoJsonLayer<Feat>({
+        id: "regions",
         data,
         filled: true,
         pickable: true,
@@ -92,6 +111,7 @@ export const Moscow = (): JSX.Element => (
         },
       }),
       new TextLayer<Feat>({
+        id: "text",
         data,
         getPosition: (f) =>
           turfCentroid(f.geometry as any).geometry.coordinates as any,
@@ -107,12 +127,13 @@ export const Moscow = (): JSX.Element => (
     initialViewState={{
       latitude: 55.751244,
       longitude: 37.618423,
-      zoom: 2,
+      zoom: 3,
       minZoom: 0,
       maxZoom: 20,
     }}
     controller={true}
     // @ts-ignore
     getTooltip={getTooltip}
+    parameters={{ cull: true }}
   />
 )
