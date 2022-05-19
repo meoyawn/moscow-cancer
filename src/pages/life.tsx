5118db163e505a58d89b576dfb75220e35d8f1f8
adelnizamutdinov <stiggpwnz@gmail.com>
2022-05-19 19:49:59 +0400

    amen

--- src/pages/life.tsx
+++ src/pages/life.tsx
@@ -1,6 +1,6 @@
 import countries from "../countries.json"
 
-import React, { useEffect, useState } from "react"
+import React, { Dispatch, useEffect, useMemo, useState } from "react"
 import DeckGL from "@deck.gl/react"
 import { GeoJsonLayer, PathLayerProps } from "@deck.gl/layers"
 import type { PickInfo } from "@deck.gl/core/lib/deck"
@@ -18,41 +18,47 @@ import type { Tooltip } from "../lib/deck"
 
 type CountryISOA3 = string & { readonly __tag__: unique symbol }
 type Year = number & { readonly __tag__: unique symbol }
-
 type Lookup = Partial<Record<CountryISOA3, Partial<Record<Year, number>>>>
+type Mode = "male" | "female" | "diff"
 
 interface Data {
   males: Lookup
   females: Lookup
-  diffToColor: (d: number) => string
 }
 
-const diff = (
-  males: Lookup,
-  females: Lookup,
+const value = (
+  { females, males }: Data,
+  mode: Mode,
   c: CountryISOA3,
   year: Year
 ): number | undefined => {
   const f = females[c]?.[year]
   const m = males[c]?.[year]
-  if (f && m) {
-    return f - m
+  switch (mode) {
+    case "diff":
+      if (f && m) {
+        return f - m
+      }
+      break
+
+    case "male":
+      return m
+
+    case "female":
+      return f
   }
 }
 
-const colorDomain = (
-  males: Lookup,
-  females: Lookup
-): [min: number, max: number] => {
+const colorDomain = (data: Data, mode: Mode): [min: number, max: number] => {
   let min = Number.MAX_SAFE_INTEGER
   let max = Number.MIN_SAFE_INTEGER
 
-  for (let c of Object.keys(males)) {
+  for (let c of Object.keys(data.males)) {
     const iso = c as CountryISOA3
-    const years = males[iso]
+    const years = data.males[iso]
     if (years) {
       for (let year of Object.keys(years)) {
-        const d = diff(males, females, iso, Number(year) as Year)
+        const d = value(data, mode, iso, Number(year) as Year)
         if (d) {
           min = Math.min(min, d)
           max = Math.max(max, d)
@@ -71,6 +77,14 @@ interface GeoProps {
 
 type Feat = Feature<Geometry, GeoProps>
 
+const interpolateColor = (data: Data, mode: Mode) => {
+  const minMax = scaleLinear()
+    .domain(colorDomain(data, mode))
+    .range(mode === "diff" ? [1, 0] : [0, 1])
+
+  return (d: number) => interpolateRdYlGn(minMax(d))
+}
+
 const fetchData = async (): Promise<Data> => {
   const [males, females] = await Promise.all(
     [fetch("/life/males.csv"), fetch("/life/females.csv")].map(x =>
@@ -78,10 +92,7 @@ const fetchData = async (): Promise<Data> => {
     )
   )
 
-  const minMax = scaleLinear().domain(colorDomain(males, females)).range([1, 0])
-  const diffToColor = (d: number) => interpolateRdYlGn(minMax(d))
-
-  return { males, females, diffToColor }
+  return { males, females }
 }
 
 const float = (s: string) => (s ? Number(s.replace(",", ".")) : undefined)
@@ -89,9 +100,6 @@ const float = (s: string) => (s ? Number(s.replace(",", ".")) : undefined)
 const parse = (csv: string) => {
   const lines = csv.split("\n")
   const columnNames = lines[0].split(";").map(Number)
-
-  let max = Number.MIN_SAFE_INTEGER
-  let min = Number.MAX_SAFE_INTEGER
 
   const ret: Lookup = {}
 
@@ -102,9 +110,6 @@ const parse = (csv: string) => {
       const year = columnNames[j] as Year
       const value = float(row[j])
       if (value) {
-        max = Math.max(max, value)
-        min = Math.min(min, value)
-
         if (!ret[code]) ret[code] = {}
         const x = ret[code]
         if (x) {
@@ -117,10 +122,11 @@ const parse = (csv: string) => {
   return ret
 }
 
-const fillColor =
-  ({ diffToColor, females, males }: Data, year: Year) =>
-  (feat: Feat): RGBAColor => {
-    const d = diff(males, females, feat.properties.ISO_A3, year)
+const fillColor = (data: Data, mode: Mode, year: Year) => {
+  const diffToColor = interpolateColor(data, mode)
+
+  return (feat: Feat): RGBAColor => {
+    const d = value(data, mode, feat.properties.ISO_A3, year)
     if (d) {
       const { r, g, b } = rgb(diffToColor(d))
       return [r, g, b]
@@ -128,6 +134,7 @@ const fillColor =
       return [150, 150, 150]
     }
   }
+}
 
 const tooltip =
   ({ females, males }: Data, year: Year) =>
@@ -140,9 +147,20 @@ const tooltip =
     return { text: `${ADMIN}\nmale ${m}\nfemale ${f}` }
   }
 
-const LifeGlobe = ({ year, data }: { year: Year; data: Data }): JSX.Element => {
-  const getFillColor = fillColor(data, year)
-  const getTooltip = tooltip(data, year)
+const LifeGlobe = ({
+  year,
+  data,
+  mode,
+}: {
+  year: Year
+  data: Data
+  mode: Mode
+}): JSX.Element => {
+  const getFillColor = useMemo(
+    () => fillColor(data, mode, year),
+    [data, mode, year]
+  )
+  const getTooltip = useMemo(() => tooltip(data, year), [data, year])
 
   return (
     <DeckGL
@@ -180,14 +198,49 @@ const LifeGlobe = ({ year, data }: { year: Year; data: Data }): JSX.Element => {
   )
 }
 
-const title = "Territories of Muscovy by year of conquest"
+const title = "Life Expectancy"
 const startYear = 1960
 const endYear = 2020
 
+const radios: ReadonlyArray<{ id: Mode; title: string }> = [
+  { id: "diff", title: "Female - Male" },
+  { id: "male", title: "Male" },
+  { id: "female", title: "Female" },
+]
+
+const Radio = ({ mode, setMode }: { mode: Mode; setMode: Dispatch<Mode> }) => (
+  <fieldset
+    className="pointer-events-auto mt-4"
+    onChange={e => setMode((e.target as HTMLInputElement).id as Mode)}
+  >
+    <legend className="sr-only">Notification method</legend>
+    <div className="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-10">
+      {radios.map(radio => (
+        <div key={radio.id} className="flex items-center">
+          <input
+            id={radio.id}
+            name="notification-method"
+            type="radio"
+            defaultChecked={mode === radio.id}
+            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
+          />
+          <label
+            htmlFor={radio.id}
+            className="ml-3 block text-sm font-medium text-gray-700"
+          >
+            {radio.title}
+          </label>
+        </div>
+      ))}
+    </div>
+  </fieldset>
+)
+
 // noinspection JSUnusedGlobalSymbols
 export default function Life(): JSX.Element {
-  const [year, setYear] = useState(endYear)
   const [data, setData] = useState<Data | null>(null)
+  const [mode, setMode] = useState(radios[0].id)
+  const [year, setYear] = useState(endYear)
 
   useEffect(() => {
     void fetchData().then(setData)
@@ -195,12 +248,14 @@ export default function Life(): JSX.Element {
 
   return (
     <div className="relative h-screen w-full overflow-hidden">
-      <SEO title={title} />
+      <SEO title={title} description="Life expectancy through space and time" />
 
-      {data ? <LifeGlobe year={year as Year} data={data} /> : null}
+      {data ? <LifeGlobe year={year as Year} data={data} mode={mode} /> : null}
 
       <div className="pointer-events-none absolute flex flex-col space-y-2 p-5">
         <h1 className="text-2xl">{title}</h1>
+
+        <Radio mode={mode} setMode={setMode} />
 
         <YearSlider
           value={year}
